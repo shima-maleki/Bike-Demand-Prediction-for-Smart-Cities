eb4e0e41c9e8f25ef604396af14a535de88f9f58
Saurabh Bhardwaj <aryan.saurabhbhardwaj@gmail.com>
2025-12-25 20:58:00 +0000

    Fix PostgreSQL initialization script
    - Removed duplicate database creation (Docker already creates it via POSTGRES_DB)
    - Fixed database name reference from bike_demand_db to bike_demand
    - Resolved container healthcheck failure on startup

--- infrastructure/postgres/init.sql
+++ infrastructure/postgres/init.sql
@@ -1,17 +1,14 @@
 -- PostgreSQL Initialization Script
 -- This script runs when the PostgreSQL container is first created
 
--- Create database
-CREATE DATABASE bike_demand_db;
+-- Note: Database is already created by Docker environment variable POSTGRES_DB
+-- Connecting to the default database (bike_demand)
 
 -- Create user (password will be set via environment variables)
 -- CREATE USER bike_demand_user WITH PASSWORD 'change_this_password';
 
--- Connect to the database
-\c bike_demand_db;
-
 -- Grant privileges
--- GRANT ALL PRIVILEGES ON DATABASE bike_demand_db TO bike_demand_user;
+-- GRANT ALL PRIVILEGES ON DATABASE bike_demand TO bike_demand_user;
 
 -- Enable required extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
